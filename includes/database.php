788c21e4151479803ed09647576b4e9194671e26
Miguel Pinto <mig06@hotmail.co.uk>
2022-04-26 13:05:04 +0200

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
-    $url='localhost';
-    $username='root';
-    $password='valdinho74';
+    $url=''; // ADD YOUR OWN
+    $username=''; // ADD YOUR OWN
+    $password=''; // ADD YOUR OWN
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $conn=mysqli_connect($url,$username,$password,"exchange");
     if(!$conn){
